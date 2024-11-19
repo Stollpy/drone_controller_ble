9caa48246eb9e18ab1ae1e891def30b2c944a70b
Stollpy <darrasflorian42@gmail.com>
2024-11-19 01:16:56 -0500

    feature: add motor controller

--- src/main.c
+++ src/main.c
@@ -1,24 +1,37 @@
+#include <stdint.h>
+#include <string.h>
+#include <stdbool.h>
 #include <stdio.h>
+#include "nvs.h"
 #include "nvs_flash.h"
-#include "esp_log.h"
+
 #include "esp_bt.h"
 #include "esp_gap_ble_api.h"
 #include "esp_gattc_api.h"
+#include "esp_gatt_defs.h"
 #include "esp_bt_main.h"
 #include "esp_gatt_common_api.h"
-#include "driver/gpio.h"
+#include "esp_log.h"
 #include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
+
+#include "driver/gpio.h"
 #include "esp_timer.h"
-#include <string.h>
 
 #define BUTTON_OFF_PIN GPIO_NUM_17
 #define BUTTON_ON_PIN GPIO_NUM_16
 
-#define TAG "BLE_CLIENT"
+#define GATTC_TAG "BLE_CLIENT"
 
-#define SERVICE_UUID 0x00FF
-#define CHARACTERISTIC_UUID 0xFF01
+#define APP_NUM 1
+
+#define APP_MOTOR_ID 0
+#define APP_MOTOR_CHAR_VAL_LEN_MAX 0x01
+#define MOTOR_SERVICE_UUID 0x00FF
+#define MOTOR_CHARACTERISTIC_UUID 0xFF01
+#define MOTOR_DESCR_UUID 0x3333
+#define MOTOR_HANDLE 0x04
+#define MOTOR_STATE_START 0x01
+#define MOTOR_STATE_STOP 0x00
 
 volatile bool button_down_pressed = false;
 volatile uint64_t last_button_down_press_time = 0;
@@ -26,11 +39,282 @@ volatile uint64_t last_button_down_press_time = 0;
 volatile bool button_on_pressed = false;
 volatile uint64_t last_button_on_press_time = 0;
 
-static esp_gattc_char_elem_t *char_elem_result = NULL;
+static char remote_device_name[ESP_BLE_ADV_DATA_LEN_MAX] = "stollpy_drone";
+static bool connect    = false;
+static bool get_server = false;
+static esp_gattc_char_elem_t *char_elem_result   = NULL;
+// static esp_gattc_descr_elem_t *descr_elem_result = NULL;
 
-static esp_gatt_if_t gattc_if_global = 0;
-static const uint8_t gatt_server_address[] = {0xd0, 0xef, 0x76, 0x1e, 0x5d, 0xee};
-static uint16_t gatt_conn_id = 0; 
+static esp_bt_uuid_t remote_filter_service_uuid = {
+    .len = ESP_UUID_LEN_16,
+    .uuid = {.uuid16 = MOTOR_SERVICE_UUID,},
+};
+
+static void esp_gattc_cb(esp_gattc_cb_event_t event, esp_gatt_if_t gattc_if, esp_ble_gattc_cb_param_t *param);
+static void gattc_motor_event_handler(esp_gattc_cb_event_t event, esp_gatt_if_t gattc_if, esp_ble_gattc_cb_param_t *param);
+
+struct gattc_app_inst {
+    esp_gattc_cb_t gattc_cb;
+    uint16_t gattc_if;
+    uint16_t app_id;
+    uint16_t conn_id;
+    uint16_t service_start_handle;
+    uint16_t service_end_handle;
+    uint16_t char_handle;
+    esp_bd_addr_t remote_bda;
+};
+
+static esp_ble_scan_params_t ble_scan_params = {
+    .scan_type              = BLE_SCAN_TYPE_ACTIVE,
+    .own_addr_type          = BLE_ADDR_TYPE_PUBLIC,
+    .scan_filter_policy     = BLE_SCAN_FILTER_ALLOW_ALL,
+    .scan_interval          = 0x50,
+    .scan_window            = 0x30
+};
+
+static struct gattc_app_inst gl_app_tab[APP_NUM] = {
+		[APP_MOTOR_ID] = {
+            .gattc_cb = gattc_motor_event_handler,
+			.gattc_if = ESP_GATT_IF_NONE,
+    },
+};
+
+static esp_bt_uuid_t remote_filter_char_uuid = {
+    .len = ESP_UUID_LEN_16,
+    .uuid = {.uuid16 = MOTOR_CHARACTERISTIC_UUID,},
+};
+
+static void gattc_motor_event_handler(esp_gattc_cb_event_t event, esp_gatt_if_t gattc_if, esp_ble_gattc_cb_param_t *param)
+{
+    esp_ble_gattc_cb_param_t *p_data = (esp_ble_gattc_cb_param_t *)param;
+
+    switch (event) {
+    case ESP_GATTC_REG_EVT:
+        ESP_LOGI(GATTC_TAG, "GATT client register, status %d, app_id %d, gattc_if %d", param->reg.status, param->reg.app_id, gattc_if);
+        esp_err_t scan_ret = esp_ble_gap_set_scan_params(&ble_scan_params);
+        if (scan_ret){
+            ESP_LOGE(GATTC_TAG, "set scan params error, error code = %x", scan_ret);
+        }
+        break;
+    case ESP_GATTC_CONNECT_EVT:{
+        ESP_LOGI(GATTC_TAG, "Connected, conn_id %d, remote "ESP_BD_ADDR_STR"", p_data->connect.conn_id,
+                 ESP_BD_ADDR_HEX(p_data->connect.remote_bda));
+        gl_app_tab[APP_MOTOR_ID].conn_id = p_data->connect.conn_id;
+        memcpy(gl_app_tab[APP_MOTOR_ID].remote_bda, p_data->connect.remote_bda, sizeof(esp_bd_addr_t));
+        esp_err_t mtu_ret = esp_ble_gattc_send_mtu_req(gattc_if, p_data->connect.conn_id);
+        if (mtu_ret){
+            ESP_LOGE(GATTC_TAG, "Config MTU error, error code = %x", mtu_ret);
+        }
+        break;
+    }
+    case ESP_GATTC_OPEN_EVT:
+        if (param->open.status != ESP_GATT_OK){
+            ESP_LOGE(GATTC_TAG, "Open failed, status %d", p_data->open.status);
+            break;
+        }
+        ESP_LOGI(GATTC_TAG, "Open successfully, MTU %u", p_data->open.mtu);
+        break;
+    case ESP_GATTC_DIS_SRVC_CMPL_EVT:
+        if (param->dis_srvc_cmpl.status != ESP_GATT_OK){
+            ESP_LOGE(GATTC_TAG, "Service discover failed, status %d", param->dis_srvc_cmpl.status);
+            break;
+        }
+        ESP_LOGI(GATTC_TAG, "Service discover complete, conn_id %d", param->dis_srvc_cmpl.conn_id);
+        esp_ble_gattc_search_service(gattc_if, param->dis_srvc_cmpl.conn_id, &remote_filter_service_uuid);
+        break;
+    case ESP_GATTC_CFG_MTU_EVT:
+        ESP_LOGI(GATTC_TAG, "MTU exchange, status %d, MTU %d", param->cfg_mtu.status, param->cfg_mtu.mtu);
+        break;
+    case ESP_GATTC_SEARCH_RES_EVT: {
+        ESP_LOGI(GATTC_TAG, "Service search result, conn_id = %x, is primary service %d", p_data->search_res.conn_id, p_data->search_res.is_primary);
+        ESP_LOGI(GATTC_TAG, "start handle %d, end handle %d, current handle value %d", p_data->search_res.start_handle, p_data->search_res.end_handle, p_data->search_res.srvc_id.inst_id);
+        if (p_data->search_res.srvc_id.uuid.len == ESP_UUID_LEN_16 && p_data->search_res.srvc_id.uuid.uuid.uuid16 == MOTOR_SERVICE_UUID) {
+            ESP_LOGI(GATTC_TAG, "Service found");
+            get_server = true;
+            gl_app_tab[APP_MOTOR_ID].service_start_handle = p_data->search_res.start_handle;
+            gl_app_tab[APP_MOTOR_ID].service_end_handle = p_data->search_res.end_handle;
+            ESP_LOGI(GATTC_TAG, "UUID16: %x", p_data->search_res.srvc_id.uuid.uuid.uuid16);
+        }
+        break;
+    }
+    case ESP_GATTC_SEARCH_CMPL_EVT:
+        if (p_data->search_cmpl.status != ESP_GATT_OK){
+            ESP_LOGE(GATTC_TAG, "Service search failed, status %x", p_data->search_cmpl.status);
+            break;
+        }
+        if(p_data->search_cmpl.searched_service_source == ESP_GATT_SERVICE_FROM_REMOTE_DEVICE) {
+            ESP_LOGI(GATTC_TAG, "Get service information from remote device");
+        } else if (p_data->search_cmpl.searched_service_source == ESP_GATT_SERVICE_FROM_NVS_FLASH) {
+            ESP_LOGI(GATTC_TAG, "Get service information from flash");
+        } else {
+            ESP_LOGI(GATTC_TAG, "Unknown service source");
+        }
+        ESP_LOGI(GATTC_TAG, "Service search complete");
+        if (get_server){
+            uint16_t count = 0;
+            esp_gatt_status_t status = esp_ble_gattc_get_attr_count(
+                gattc_if,
+                p_data->search_cmpl.conn_id,
+                ESP_GATT_DB_CHARACTERISTIC,
+                gl_app_tab[APP_MOTOR_ID].service_start_handle,
+                gl_app_tab[APP_MOTOR_ID].service_end_handle,
+                ESP_GATT_INVALID_HANDLE,
+                &count);
+            if (status != ESP_GATT_OK){
+                ESP_LOGE(GATTC_TAG, "esp_ble_gattc_get_attr_count error");
+                break;
+            }
+
+            if (count > 0){
+                char_elem_result = (esp_gattc_char_elem_t *)malloc(sizeof(esp_gattc_char_elem_t) * count);
+                if (!char_elem_result){
+                    ESP_LOGE(GATTC_TAG, "gattc no mem");
+                    break;
+                }else{
+                    status = esp_ble_gattc_get_char_by_uuid(
+                        gattc_if,
+                        p_data->search_cmpl.conn_id,
+                        gl_app_tab[APP_MOTOR_ID].service_start_handle,
+                        gl_app_tab[APP_MOTOR_ID].service_end_handle,
+                        remote_filter_char_uuid,
+                        char_elem_result,
+                        &count);
+                    if (status != ESP_GATT_OK){
+                        ESP_LOGE(GATTC_TAG, "esp_ble_gattc_get_char_by_uuid error");
+                        free(char_elem_result);
+                        char_elem_result = NULL;
+                        break;
+                    }
+
+                    if (count > 0 && (char_elem_result[0].properties & ESP_GATT_CHAR_PROP_BIT_WRITE)){
+                        gl_app_tab[APP_MOTOR_ID].char_handle = char_elem_result[0].char_handle;
+                    }
+                }
+                free(char_elem_result);
+            }else{
+                ESP_LOGE(GATTC_TAG, "no char found");
+            }
+        }
+         break;
+    case ESP_GATTC_REG_FOR_NOTIFY_EVT:
+        break;
+    case ESP_GATTC_NOTIFY_EVT:
+        break;
+    default:
+        break;
+    }
+}
+
+static void esp_gap_cb(esp_gap_ble_cb_event_t event, esp_ble_gap_cb_param_t *param)
+{
+    uint8_t *adv_name = NULL;
+    uint8_t adv_name_len = 0;
+    switch (event) {
+    case ESP_GAP_BLE_SCAN_PARAM_SET_COMPLETE_EVT: {
+        uint32_t duration = 30;
+        esp_ble_gap_start_scanning(duration);
+        break;
+    }
+    case ESP_GAP_BLE_SCAN_START_COMPLETE_EVT:
+        if (param->scan_start_cmpl.status != ESP_BT_STATUS_SUCCESS) {
+            ESP_LOGE(GATTC_TAG, "Scanning start failed, status %x", param->scan_start_cmpl.status);
+            break;
+        }
+        ESP_LOGI(GATTC_TAG, "Scanning start successfully");
+
+        break;
+    case ESP_GAP_BLE_SCAN_RESULT_EVT: {
+        esp_ble_gap_cb_param_t *scan_result = (esp_ble_gap_cb_param_t *)param;
+        switch (scan_result->scan_rst.search_evt) {
+        case ESP_GAP_SEARCH_INQ_RES_EVT:
+            adv_name = esp_ble_resolve_adv_data(
+                scan_result->scan_rst.ble_adv,
+                ESP_BLE_AD_TYPE_NAME_CMPL,
+                &adv_name_len);
+
+            ESP_LOGI(GATTC_TAG, "Scan result, device "ESP_BD_ADDR_STR", name len %u", ESP_BD_ADDR_HEX(scan_result->scan_rst.bda), adv_name_len);
+            ESP_LOG_BUFFER_CHAR(GATTC_TAG, adv_name, adv_name_len);
+
+            if (adv_name != NULL) {
+                if (strlen(remote_device_name) == adv_name_len && strncmp((char *)adv_name, remote_device_name, adv_name_len) == 0) {
+                    ESP_LOGI(GATTC_TAG, "Device found %s", remote_device_name);
+                    if (connect == false) {
+                        connect = true;
+                        ESP_LOGI(GATTC_TAG, "Connect to the remote device");
+                        esp_ble_gap_stop_scanning();
+                        esp_ble_gattc_open(gl_app_tab[APP_MOTOR_ID].gattc_if, scan_result->scan_rst.bda, scan_result->scan_rst.ble_addr_type, true);
+                    }
+                }
+            }
+            break;
+        case ESP_GAP_SEARCH_INQ_CMPL_EVT:
+            // if server device not found, research here
+            break;
+        default:
+            break;
+        }
+        break;
+    }
+
+    case ESP_GAP_BLE_SCAN_STOP_COMPLETE_EVT:
+        if (param->scan_stop_cmpl.status != ESP_BT_STATUS_SUCCESS){
+            ESP_LOGE(GATTC_TAG, "Scanning stop failed, status %x", param->scan_stop_cmpl.status);
+            break;
+        }
+        ESP_LOGI(GATTC_TAG, "Scanning stop successfully");
+        break;
+
+    case ESP_GAP_BLE_ADV_STOP_COMPLETE_EVT:
+        if (param->adv_stop_cmpl.status != ESP_BT_STATUS_SUCCESS){
+            ESP_LOGE(GATTC_TAG, "Advertising stop failed, status %x", param->adv_stop_cmpl.status);
+            break;
+        }
+        ESP_LOGI(GATTC_TAG, "Advertising stop successfully");
+        break;
+    case ESP_GAP_BLE_UPDATE_CONN_PARAMS_EVT:
+         ESP_LOGI(GATTC_TAG, "Connection params update, status %d, conn_int %d, latency %d, timeout %d",
+                  param->update_conn_params.status,
+                  param->update_conn_params.conn_int,
+                  param->update_conn_params.latency,
+                  param->update_conn_params.timeout);
+        break;
+    case ESP_GAP_BLE_SET_PKT_LENGTH_COMPLETE_EVT:
+        ESP_LOGI(GATTC_TAG, "Packet length update, status %d, rx %d, tx %d",
+                  param->pkt_data_length_cmpl.status,
+                  param->pkt_data_length_cmpl.params.rx_len,
+                  param->pkt_data_length_cmpl.params.tx_len);
+        break;
+    default:
+        break;
+    }
+}
+
+static void esp_gattc_cb(esp_gattc_cb_event_t event, esp_gatt_if_t gattc_if, esp_ble_gattc_cb_param_t *param)
+{
+    ESP_LOGI(GATTC_TAG, "EVT %d, gattc if %d", event, gattc_if);
+
+    if (event == ESP_GATTC_REG_EVT) {
+        if (param->reg.status == ESP_GATT_OK) {
+            gl_app_tab[param->reg.app_id].gattc_if = gattc_if;
+        } else {
+            ESP_LOGI(GATTC_TAG, "reg app failed, app_id %04x, status %d",
+                    param->reg.app_id,
+                    param->reg.status);
+            return;
+        }
+    }
+
+    do {
+        int idx;
+        for (idx = 0; idx < APP_NUM; idx++) {
+            if (gattc_if == ESP_GATT_IF_NONE || gattc_if == gl_app_tab[idx].gattc_if) {
+                if (gl_app_tab[idx].gattc_cb) {
+                    gl_app_tab[idx].gattc_cb(event, gattc_if, param);
+                }
+            }
+        }
+    } while (0);
+}
 
 void IRAM_ATTR button_off_isr_handler(void* arg) {
         uint64_t current_time = esp_timer_get_time();  
@@ -45,45 +329,6 @@ void IRAM_ATTR button_on_isr_handler(void* arg) {
     if (current_time - last_button_on_press_time > 50000) {
         button_on_pressed = true;
         last_button_on_press_time = current_time;
-    }
-}
-
-static void gap_event_handler(esp_gap_ble_cb_event_t event, esp_ble_gap_cb_param_t *param) {
-    switch (event) {
-        case ESP_GAP_BLE_SCAN_RESULT_EVT:
-            if (param->scan_rst.search_evt == ESP_GAP_SEARCH_INQ_RES_EVT) {
-                if (memcmp(param->scan_rst.bda, gatt_server_address, 6) == 0) {
-                    ESP_LOGI(TAG, "Serveur GATT trouvé, connexion en cours...");
-                    esp_ble_gap_stop_scanning();
-                    esp_ble_gattc_open(gattc_if_global, param->scan_rst.bda, BLE_ADDR_TYPE_PUBLIC, true);
-                }
-            }
-            break;
-        default:
-            break;
-    }
-}
-
-void gattc_event_handler(esp_gattc_cb_event_t event, esp_gatt_if_t gattc_if, esp_ble_gattc_cb_param_t *param) {
-    switch (event) {
-        case ESP_GATTC_REG_EVT:
-            printf("Client GATT saved !");
-            ESP_LOGI(TAG, "Client GATT saved !");
-            gattc_if_global = gattc_if;
-            esp_ble_gap_start_scanning(10);
-            break;
-        case ESP_GATTC_OPEN_EVT:
-            printf("Connexion établie avec le serveur GATT");
-            ESP_LOGI(TAG, "Connexion établie avec le serveur GATT");
-            gatt_conn_id = param->open.conn_id;  // Sauvegarder conn_id lorsque la connexion est ouverte
-            esp_ble_gattc_search_service(gattc_if, gatt_conn_id, NULL);
-            break;
-        case ESP_GATTC_SEARCH_CMPL_EVT:
-            ESP_LOGI(TAG, "Search is finish");
-            printf("Search is finish \n");
-            break;
-        default:
-            break;
     }
 }
 
@@ -117,25 +362,52 @@ void init_ble() {
     }
     ESP_ERROR_CHECK(ret);
 
-    // Libérer la mémoire du mode Classic BT
-    ESP_ERROR_CHECK(esp_bt_controller_mem_release(ESP_BT_MODE_CLASSIC_BT));
-
-    // Configurer le contrôleur Bluetooth
     esp_bt_controller_config_t bt_cfg = BT_CONTROLLER_INIT_CONFIG_DEFAULT();
-    ESP_ERROR_CHECK(esp_bt_controller_init(&bt_cfg));
-    ESP_ERROR_CHECK(esp_bt_controller_enable(ESP_BT_MODE_BLE));
+    ret = esp_bt_controller_init(&bt_cfg);
+    if (ret) {
+        ESP_LOGE(GATTC_TAG, "%s initialize controller failed, error code = %x", __func__, ret);
+        return;
+    }
 
-    // Initialiser le Bluedroid
-    ESP_ERROR_CHECK(esp_bluedroid_init());
-    ESP_ERROR_CHECK(esp_bluedroid_enable());
+    ret = esp_bt_controller_enable(ESP_BT_MODE_BLE);
+    if (ret) {
+        ESP_LOGE(GATTC_TAG, "%s enable controller failed, error code = %x", __func__, ret);
+        return;
+    }
 
-    // Enregistrer les callbacks GAP et GATT
-    ESP_ERROR_CHECK(esp_ble_gap_register_callback(gap_event_handler));
-    ESP_ERROR_CHECK(esp_ble_gattc_register_callback(gattc_event_handler));
+    ret = esp_bluedroid_init();
+    if (ret) {
+        ESP_LOGE(GATTC_TAG, "%s init bluetooth failed, error code = %x", __func__, ret);
+        return;
+    }
 
-    // Définir le nom de l’appareil et démarrer le scanning
-    esp_ble_gap_set_device_name("stolly_drone_controller");
-    ESP_ERROR_CHECK(esp_ble_gap_start_scanning(10));
+    ret = esp_bluedroid_enable();
+    if (ret) {
+        ESP_LOGE(GATTC_TAG, "%s enable bluetooth failed, error code = %x", __func__, ret);
+        return;
+    }
+
+    ret = esp_ble_gap_register_callback(esp_gap_cb);
+    if (ret){
+        ESP_LOGE(GATTC_TAG, "%s gap register failed, error code = %x", __func__, ret);
+        return;
+    }
+
+    ret = esp_ble_gattc_register_callback(esp_gattc_cb);
+    if(ret){
+        ESP_LOGE(GATTC_TAG, "%s gattc register failed, error code = %x", __func__, ret);
+        return;
+    }
+
+    ret = esp_ble_gattc_app_register(APP_MOTOR_ID);
+    if (ret){
+        ESP_LOGE(GATTC_TAG, "%s gattc app register failed, error code = %x", __func__, ret);
+    }
+
+    esp_err_t local_mtu_ret = esp_ble_gatt_set_local_mtu(500);
+    if (local_mtu_ret){
+        ESP_LOGE(GATTC_TAG, "set local  MTU failed, error code = %x", local_mtu_ret);
+    }
 
     printf("INIT BLE DONE \n");
 }
@@ -151,9 +423,9 @@ void app_main() {
         if (button_down_pressed) {
             printf("Buton off pressed !\n");
             
-            uint8_t value = 1;
-            esp_ble_gattc_write_char(gattc_if_global, gatt_conn_id, char_elem_result->char_handle, sizeof(value), &value, ESP_GATT_WRITE_TYPE_RSP, ESP_GATT_AUTH_REQ_NONE);
-            ESP_LOGI(TAG, "Notification of pressed is send");
+            uint8_t value = 0;
+            esp_ble_gattc_write_char(gl_app_tab[APP_MOTOR_ID].gattc_if, gl_app_tab[APP_MOTOR_ID].conn_id, gl_app_tab[APP_MOTOR_ID].char_handle, sizeof(value), &value, ESP_GATT_WRITE_TYPE_RSP, ESP_GATT_AUTH_REQ_NONE);
+            ESP_LOGI(GATTC_TAG, "Notification of pressed is send");
 
             button_down_pressed = false;
         }
@@ -162,8 +434,8 @@ void app_main() {
             printf("Buton on pressed !\n");
 
             uint8_t value = 1;
-            esp_ble_gattc_write_char(gattc_if_global, gatt_conn_id, char_elem_result->char_handle, sizeof(value), &value, ESP_GATT_WRITE_TYPE_RSP, ESP_GATT_AUTH_REQ_NONE);
-             ESP_LOGI(TAG, "Notification of pressed is send");
+            esp_ble_gattc_write_char(gl_app_tab[APP_MOTOR_ID].gattc_if, gl_app_tab[APP_MOTOR_ID].conn_id, gl_app_tab[APP_MOTOR_ID].char_handle, sizeof(value), &value, ESP_GATT_WRITE_TYPE_RSP, ESP_GATT_AUTH_REQ_NONE);
+             ESP_LOGI(GATTC_TAG, "Notification of pressed is send");
 
             button_on_pressed = false;
         }
